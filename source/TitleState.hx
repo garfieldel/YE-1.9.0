8ed829920ff13161e1e80cbb1bfdc5142c624fbd
garfieldel <closetlionel@gmail.com>
2026-05-19 20:01:59 -0500

    Update TitleState.hx

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -369,8 +369,6 @@ class TitleState extends MusicBeatState
 		ngSpr.antialiasing = true;
 
 		FlxTween.tween(credTextShit, {y: credTextShit.y + 20}, 2.9, {ease: FlxEase.quadInOut, type: PINGPONG});
-
-		FlxG.mouse.visible = true;
 		
 		updateRibbon = new FlxSprite(0, FlxG.height - 75).makeGraphic(FlxG.width, 75, 0x88000000);
 		updateRibbon.visible = false;
