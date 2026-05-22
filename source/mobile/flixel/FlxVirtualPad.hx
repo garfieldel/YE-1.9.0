e36fbc6f8c7bdc963b80a27d6ffb15d10140017c
garfieldel <closetlionel@gmail.com>
2026-05-22 15:37:24 -0500

    Update FlxVirtualPad.hx

--- source/mobile/flixel/FlxVirtualPad.hx
+++ source/mobile/flixel/FlxVirtualPad.hx
@@ -89,7 +89,7 @@ class FlxVirtualPad extends FlxSpriteGroup {
 				dPad.add(add(buttonB = createButton(FlxG.width - 258, FlxG.height - 135, 132, 127, "b", 0xFFCB00)));
 				dPad.add(add(buttonX = createButton(FlxG.width - 132, FlxG.height - 135, 132, 127, "x", 0xFF0000)));
 			case D_X:
-				dPad.add(add(buttonB = createButton(FlxG.width - 258, FlxG.height - 135, 132, 127, "d", 0xFFCB00)));
+				dPad.add(add(buttonD = createButton(FlxG.width - 258, FlxG.height - 135, 132, 127, "d", 0xFFCB00)));
 				dPad.add(add(buttonX = createButton(FlxG.width - 132, FlxG.height - 135, 132, 127, "x", 0xFF0000)));
 			case A_B_C:
 				dPad.add(add(buttonC = createButton(FlxG.width - 384, FlxG.height - 135, 132, 127, "c", 0x44FF00)));
