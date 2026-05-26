37e8da543ba65009e8f2ad8ae3582d2ecfd18a8d
garfieldel <closetlionel@gmail.com>
2026-05-26 16:05:40 -0500

    Update ModMenuState.hx

--- source/ModMenuState.hx
+++ source/ModMenuState.hx
@@ -71,9 +71,11 @@ class ModMenuState extends MusicBeatState {
         if (controls.DOWN_P #if mobile || _dpad.buttonDown.justPressed #end) {
             changeSelection(1);
         }
+        #if !mobile
         if (FlxG.mouse.wheel != 0) {
             changeSelection(-FlxG.mouse.wheel);
         }
+        #end
         if (controls.BACK #if mobile || FlxG.android.justReleased.BACK #end) {
             FlxG.switchState(new MainMenuState());
         }
