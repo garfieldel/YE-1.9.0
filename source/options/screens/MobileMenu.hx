e89bc7f2d965c47101ced985ddd58e8f305b88c4
garfieldel <closetlionel@gmail.com>
2026-06-11 17:03:40 -0500

    Update MobileMenu.hx

--- source/options/screens/MobileMenu.hx
+++ source/options/screens/MobileMenu.hx
@@ -7,11 +7,20 @@ class MobileMenu extends OptionScreen {
     public override function create() {
         options = [
             {
-                name: "Hitbox Opacity",
-                desc: "FUCK MY ASS STUPID NIGGER",
-                value: "",
-                onCreate: function(e) {e.check(Settings.engineSettings.data.hitboxopacity);},
-                onSelect: function(e) {e.check(Settings.engineSettings.data.hitboxopacity = !Settings.engineSettings.data.hitboxopacity);},
+                name: "FUCK HITBOX OPACITY",
+                desc: "fck.",
+                value: '${Settings.engineSettings.data.hitboxOpacity} FCK',
+                onLeft: function(e) {e.value = '${Settings.engineSettings.data.hitboxOpacity} FCK';},
+                onUpdate: function(e) {
+                    if (controls.LEFT_P #if mobile || _dpad.buttonLeft.justPressed #end) Settings.engineSettings.data.hitboxOpacity -= 10;
+                    if (controls.RIGHT_P #if mobile || _dpad.buttonRight.justPressed #end) Settings.engineSettings.data.hitboxOpacity += 10;
+                    Settings.engineSettings.data.hitboxOpacity = Std.int(FlxMath.bound(Settings.engineSettings.data.hitboxOpacity, 20, 300));
+                    
+					FlxG.drawFramerate = Settings.engineSettings.data.fpsCap;
+					FlxG.updateFramerate = Settings.engineSettings.data.fpsCap;
+                    
+                    e.value = '< ${Settings.engineSettings.data.fpsCap} FPS >';
+                }
             },
             {
                 name: "Green Screen Mode",
