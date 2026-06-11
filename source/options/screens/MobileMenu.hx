3963b70166242afd0fcb613f57ca53439557c7b1
garfieldel <closetlionel@gmail.com>
2026-06-11 17:30:50 -0500

    Update MobileMenu.hx

--- source/options/screens/MobileMenu.hx
+++ source/options/screens/MobileMenu.hx
@@ -7,21 +7,34 @@ class MobileMenu extends OptionScreen {
     public override function create() {
         options = [
             {
-                name: "FUCK HITBOX OPACITY",
-                desc: "fck.",
-                value: '${Settings.engineSettings.data.hitboxOpacity} FCK',
-                onLeft: function(e) {e.value = '${Settings.engineSettings.data.hitboxOpacity} FCK';},
-                onUpdate: function(e) {
-                    if (controls.LEFT_P #if mobile || _dpad.buttonLeft.justPressed #end) Settings.engineSettings.data.hitboxOpacity -= 10;
-                    if (controls.RIGHT_P #if mobile || _dpad.buttonRight.justPressed #end) Settings.engineSettings.data.hitboxOpacity += 10;
-                    Settings.engineSettings.data.hitboxOpacity = Std.int(FlxMath.bound(Settings.engineSettings.data.hitboxOpacity, 20, 300));
-                    
-					FlxG.drawFramerate = Settings.engineSettings.data.hitboxOpacity;
-					FlxG.updateFramerate = Settings.engineSettings.data.hitboxOpacity;
-                    
-                    e.value = '< ${Settings.engineSettings.data.hitboxOpacity} FCK >';
-                }
-            }
+    name: "Hitbox Opacity",
+    desc: "Adjust the opacity of the hitboxes. Range: 0.0 - 1.0",
+    value: "",
+    onCreate: function(e) {
+        e.value = Std.string(Settings.engineSettings.data.hitboxOpacity);
+    },
+
+    onUpdate: function(e) {
+        var oldValue = Settings.engineSettings.data.hitboxOpacity;
+
+        if (controls.LEFT_P #if mobile || _dpad.buttonLeft.justPressed #end)
+            Settings.engineSettings.data.hitboxOpacity -= 0.1;
+
+        if (controls.RIGHT_P #if mobile || _dpad.buttonRight.justPressed #end)
+            Settings.engineSettings.data.hitboxOpacity += 0.1;
+
+        // Limitar entre 0 y 1
+        Settings.engineSettings.data.hitboxOpacity =
+            FlxMath.bound(
+                FlxMath.roundDecimal(Settings.engineSettings.data.hitboxOpacity, 1),
+                0,
+                1
+            );
+
+        if (oldValue != Settings.engineSettings.data.hitboxOpacity)
+            e.value = '< ${Settings.engineSettings.data.hitboxOpacity} >';
+    }
+}
         ];
         super.create();
     }
