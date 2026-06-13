4e3561fe5d14b482693069b1106c209a9954b9ce
garfieldel <closetlionel@gmail.com>
2026-06-12 19:10:51 -0500

    Update MobileMenu.hx

--- source/options/screens/MobileMenu.hx
+++ source/options/screens/MobileMenu.hx
@@ -17,14 +17,14 @@ class MobileMenu extends OptionScreen {
 
                     if (controls.LEFT_P #if mobile || _dpad.buttonLeft.justPressed #end) Settings.engineSettings.data.hitboxOpacity -= 0.1;
                     if (controls.RIGHT_P #if mobile || _dpad.buttonRight.justPressed #end) Settings.engineSettings.data.hitboxOpacity += 0.1;
-                    Settings.engineSettings.data.hitboxOpacity = FlxMath.bound(FlxMath.roundDecimal(Settings.engineSettings.data.hitboxOpacity, 1), 0, 10);
+                    Settings.engineSettings.data.hitboxOpacity = FlxMath.bound(FlxMath.roundDecimal(Settings.engineSettings.data.hitboxOpacity, 1), 0, 1);
                     if (oldValue != Settings.engineSettings.data.hitboxOpacity)
                         e.value = '< ${Settings.engineSettings.data.hitboxOpacity} >';
                 }
             },
             {
                 name: "Hitbox Hidden",
-                desc: "If true, the hitboxes will disappear; if false, nothing will change; use true or false.",
+                desc: "If on, the hitboxes will disappear; if off, nothing will change",
                 value: "",
                 onCreate: function(e) {e.check(Settings.engineSettings.data.hitboxHidden);},
                 onSelect: function(e) {e.check(Settings.engineSettings.data.hitboxHidden = !Settings.engineSettings.data.hitboxHidden);},
